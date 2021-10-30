58ecc772c10d0f1c3e44efde409e46679069e00d
Moniruzzaman Shanto <42086179+Shanto75@users.noreply.github.com>
2021-10-30 10:55:01 +0600

    Update db.php

--- bb/db.php
+++ bb/db.php
@@ -1,12 +1,6 @@
 <?php
 // Connecting to the Database
-    // $servername = "sql203.epizy.com";
-    // $username = "epiz_29638026";
-    // $password = "VY4FF2mG2ZIH";
-    // $database = "epiz_29638026_bbms";
-
-    // $conn = mysqli_connect($servername, $username, $password, $database);
-
+   
     $servername = "localhost";
     $username = "root";
     $password = "";
